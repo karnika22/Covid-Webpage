6d852b8576b16c028a8167aee54ce42797480fcd
Karnika <karnikarajput1122@gmail.com>
2021-11-18 19:57:18 +0530

    api work

--- index.js
+++ index.js
@@ -4,16 +4,7 @@ async function fetchData(){
     let reg=result.data.summary;
     //console.log(reg.total, reg.recovered, reg.deaths, reg.active)
     console.log(reg.total, reg.deaths)
-}
+        let parent=document.getElementsByClassName("text");
+        parent.append(reg.total);
+    }
 fetchData();
-let parent=document.getElementById("res");
-
-        let dtlist=document.createElement("datalist");
-        dtlist.setAttribute("id","myStateList");
-        parent.append(dtlist);
-        for(let i=0;i<reg.length;i++){
-            let option=document.createElement("option");
-
-  option.setAttribute("value",reg[i].loc);
-            dtlist.append(option);
-        }
