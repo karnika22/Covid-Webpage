9a5173c5923fcd68313fae521ddd25babe76b220
Karnika <karnikarajput1122@gmail.com>
2021-11-20 19:05:07 +0530

    data fetched

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
     async function fetchData(){
         let response=await fetch("https://api.rootnet.in/covid19-in/stats/latest")
         let result=await response.json();
-        let reg=result.data.summary;
-
+        let reg=result.data["unofficial-summary"][0];
+        console.log(reg)
         let total=document.querySelector("#total");
-        total.innerHTML=`<div> <h2> ${reg.total}</h2></div>`
+        total.innerHTML=`<h2> ${reg.total}</h2>`
 
         let death=document.querySelector("#death");
-        death.innerHTML=`<div> <h2> ${reg.deaths}</h2></div>`
+        death.innerHTML=` <h2> ${reg.deaths}</h2>`
+
+        let recovered=document.querySelector("#recovered");
+        recovered.innerHTML=` <h2> ${reg.recovered}</h2>`
+
+        let active=document.querySelector("#active");
+        active.innerHTML=` <h2> ${reg.active}</h2>`
 
 
     }
